3bf61995ad2222e4d62c1448ffbe85a12d9feab4
ebertra <emmanuel.bv@gmail.com>
2014-11-29 17:17:52 +0100

    Working app with NFC and uri handler before refactor

--- NFCTimer-SL/MainPage.xaml.cs
+++ NFCTimer-SL/MainPage.xaml.cs
@@ -10,10 +10,21 @@ using Microsoft.Phone.Shell;
 using NFCTimer_SL.Resources;
 using NFCTimer_SL.ViewModel;
 
+using Windows.Networking.Proximity;
+
+//using Microsoft.Xna.Framework.Audio;//sound effect
+
+//extend type : ByteArray.AsBuffer()
+using System.Runtime.InteropServices.WindowsRuntime;
+//encoding/decoding string to ByteArray
+using System.Text;
+
 namespace NFCTimer_SL
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        ProximityDevice device { get; set; }
+
         // Constructor
         public MainPage()
         {
@@ -25,7 +36,92 @@ namespace NFCTimer_SL
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+
+            var appId = Windows.ApplicationModel.Store.CurrentApp.AppId;
+            
+            //Subscribe to NFC Events
+            //TODO: all this should be in a separate, reusable class
+            Loaded += MainPage_Loaded;
         }
+
+        //TO DO: Move to a real URI Mapper or Route Classs
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            string parameter = string.Empty;
+            if (NavigationContext.QueryString.TryGetValue("ms_nfp_launchargs", out parameter))
+            {
+                //MessageBox.Show("Congratulation\nYou launch application with a NFC tag.\nParamaters : " + parameter);
+                //NavigationContext.QueryString.Remove("ms_nfp_launchargs");
+                if (parameter == "action=startstop")
+                {
+                    MainViewModel mainViewModel = this.DataContext as MainViewModel; 
+                    if ((mainViewModel != null) && (mainViewModel.StartStopTimerCommand.CanExecute(null)))
+                        mainViewModel.StartStopTimerCommand.Execute(null);
+                }
+            }
+        }
+
+        #region Initialisation
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+            if (device == null)
+            {
+                //get default NFC device
+                device = ProximityDevice.GetDefault();
+                device.DeviceArrived += device_DeviceArrived;
+                device.DeviceDeparted += device_DeviceDeparted;
+
+                device.SubscribeForMessage("WindowsUri", WindowsUriHandler);
+
+            }
+        }
+
+        void device_DeviceDeparted(ProximityDevice sender)
+        {
+            //Update the UI
+            //beep
+        }
+
+        void device_DeviceArrived(ProximityDevice sender)
+        {
+            //Update the UI
+        }
+
+        #endregion
+
+        #region NFC EVENT HANDLER
+        //URI message received
+        private void WindowsUriHandler(ProximityDevice sender, ProximityMessage message)
+        {
+            try
+            {
+                var buffer = message.Data.ToArray();
+                var sUri = Encoding.Unicode.GetString(buffer, 0, buffer.Length);
+
+                //remove null charater if present
+                if (sUri[sUri.Length - 1] == '\0')
+                    sUri = sUri.Remove(sUri.Length - 1);
+
+                var uri = new Uri(sUri);
+                string s = "WindowsUri : \n" + uri + "\n\n";
+                Dispatcher.BeginInvoke(() =>
+                {
+                    MessageBox.Show("Go to: "+s);
+                    //update the UI
+                });
+            }
+            catch (Exception e)
+            {
+                Dispatcher.BeginInvoke(() =>
+                {
+                    MessageBox.Show(e.Message);
+                });
+            }
+        }
+        #endregion
 
         // Sample code for building a localized ApplicationBar
         //private void BuildLocalizedApplicationBar()
---
+++ NFCTimer-SLTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace NFCTimer_SLTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //[TestMethod]
+        //public async void TestMethod1()
+        //{
+        //    await Windows.System.Launcher.LaunchUriAsync(new System.Uri("nfctimer:action=startstop"));
+        //}
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(System.Uri.IsWellFormedUriString("/Protocol?encodedLaunchUri=nfctimer%3Aaction%3Dstartstop", System.UriKind.RelativeOrAbsolute));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(System.Uri.IsWellFormedUriString("/Protocol?encodedLaunchUri=nfctimer%3Aaction%3Dstartstop", System.UriKind.Absolute));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var uri = new Uri("/Protocol?encodedLaunchUri=nfctimer%3Aaction%3Dstartstop");
+            Assert.IsTrue(true);
+        }
+    }
+}
